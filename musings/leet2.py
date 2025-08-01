7a0caa6b6ae123c37e855e9fb7d953599c3dcbc2
anovero123 <novero.a@gmail.com>
2025-08-01 13:16:55 -0400

    meh

--- musings/leet2.py
+++ musings/leet2.py
@@ -54,9 +54,38 @@ def longestCommonPrefix(arr):
         # Return the common prefix
         return first[:i]
 
+def removeInPlace(nums, val):
+    numP = nums
+    numP[:] = list(filter(lambda item: item !=val, numP))
+
+    for x in range(len(nums)-1, -1, -1):
+            print(nums[x], nums)
+            if nums[x] == val:
+                nums.pop(x)
+    print(f"lComp: {numP}, loop backward : {nums}")
+
+from itertools import permutations
+def findSubstring(s, words):
+    # You are given a string s and an array of strings words. All the strings of words are of the same length.
+
+    # A concatenated string is a string that exactly contains all the strings of any permutation of words concatenated.
+
+    # For example, if words = ["ab","cd","ef"], then "abcdef", "abefcd", "cdabef", "cdefab", "efabcd", and "efcdab" are all concatenated strings. "acdbef" is not a concatenated string because it is not the concatenation of any permutation of words.
+    # Return an array of the starting indices of all the concatenated substrings in s. You can return the answer in any order.
+    res = []
+    for i in range(len(words), len(words)+1):
+        res.extend(list(permutations(words, i)))
+    res[:] = ["".join(e) for e in res]
+    idx2 = []
+    for w in res:
+         idx2.extend([i for i in range(len(s)) if s.startswith(w,i)])
+    return list(set(idx2))
 if __name__ == '__main__':
     print(reverseS( ["h","e","l","l","o"]))
     print(reverseInt(123), reverseInt(-123))
     print(findUnique('leetcode'))
     print(findNeedle('hello', "ll"))
     print(longestCommonPrefix(["flower","flow","flight"]))
+    removeInPlace([0,1,2,2,3,0,4,2], 2)
+    print(findSubstring("wordgoodgoodgoodbestword", ["word","good","best","good"]))
+    print(findSubstring("foobarfoobar", ["foo","bar"]))
